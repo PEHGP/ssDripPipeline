ba37b17093e6685df0b751356e6099fe039a31ca
PEHGP <bigkk913@126.com>
2019-02-26 14:36:31 +0800

    add main

--- bin/ssDRIPSeqAnalysis.py
+++ bin/ssDRIPSeqAnalysis.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python2.7
+from piplines import DripMain
+import sys
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		print "ssDRIPSeqAnalysis.py <DripConfig.json> <BaseAnalysis|DownstreamAnalysis|AllPip>"
+		sys.exit()
+	ConfigFile=sys.argv[1]
+	SubCommand=sys.argv[2] 	
+	DripMain.Main(ConfigFile,SubCommand)
