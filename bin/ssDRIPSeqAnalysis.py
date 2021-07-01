88ff663b032f2420f15a7fa61c3bc03128281cba
PEHGP <396777306@qq.com>
2021-07-01 12:42:34 +0800

    add DeseqAnalysis

--- bin/ssDRIPSeqAnalysis.py
+++ bin/ssDRIPSeqAnalysis.py
@@ -3,8 +3,8 @@ from piplines import DripMain
 import sys
 if __name__ == "__main__":
 	if len(sys.argv) == 1:
-		print "ssDRIPSeqAnalysis.py <DripConfig.json> <BaseAnalysis|DownstreamAnalysis|AllPip>"
+		print "ssDRIPSeqAnalysis.py <DripConfig.json> <BaseAnalysis|DeseqAnalysis|DownstreamAnalysis|AllPip>"
 		sys.exit()
 	ConfigFile=sys.argv[1]
-	SubCommand=sys.argv[2] 	
+	SubCommand=sys.argv[2]
 	DripMain.Main(ConfigFile,SubCommand)
